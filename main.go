92920691bde1bce9d611e247d7d2d55da5c69368
Shinatana <167446715+Shinatana@users.noreply.github.com>
2024-05-19 17:12:58 +0300

    Update main.go

--- main.go
+++ main.go
@@ -61,7 +61,7 @@ func (t Training) TrainingInfo() InfoMessage {
 		TrainingType: t.TrainingType,
 		Duration:     t.Duration,
 		Distance:     t.distance(),
-		MeanSpeed:    t.MeanSpeed(),
+		speed:    t.MeanSpeed(),
 		Calories:     t.Calories(),
 	}
 	return inf
